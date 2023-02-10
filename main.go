b05ae91c9cd727bab1e04c507aa3e8aab84cabdc
Ron Appleton <ron.appleton@gmail.com>
2023-02-10 23:49:12 +0000

    Updates

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 )
 
 type config struct {
-	port       string
-	filesPath  string
-	production bool
-	domain     string
-	email      string
+	port          string
+	storagePath   string
+	storageFolder string
+	production    bool
+	domain        string
+	email         string
 }
 
 func (c *config) init(args []string) error {
@@ -28,11 +29,12 @@ func (c *config) init(args []string) error {
 	}
 
 	var (
-		port       = flags.String("port", "80", "The port this server should run on")
-		filesPath  = flags.String("filesPath", filepath.Dir(ex), "Full path to file folder location")
-		production = flags.Bool("production", false, "Run server in production mode")
-		domain     = flags.String("domain", "", "The servers domain name")
-		email      = flags.String("email", "", "The servers domain administrator")
+		port          = flags.String("port", "80", "The port this server should run on")
+		filesPath     = flags.String("storagePath", filepath.Dir(ex), "Full path to file folder location")
+		storageFolder = flags.String("storageFolder", "files", "The outer most folder")
+		production    = flags.Bool("production", false, "Run server in production mode")
+		domain        = flags.String("domain", "", "The servers domain name")
+		email         = flags.String("email", "", "The servers domain administrator")
 	)
 
 	if err := flags.Parse(args[1:]); err != nil {
@@ -40,7 +42,8 @@ func (c *config) init(args []string) error {
 	}
 
 	c.port = *port
-	c.filesPath = *filesPath
+	c.storagePath = *filesPath
+	c.storageFolder = *storageFolder
 	c.production = *production
 	c.domain = *domain
 	c.email = *email
@@ -50,12 +53,14 @@ func (c *config) init(args []string) error {
 
 func main() {
 	config := &config{}
-	config.init(os.Args)
+	_ = config.init(os.Args)
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
 
 	app := iris.New()
+	app.Use(iris.Cache304(24 * time.Hour))
+	app.Use(iris.Cache(720 * time.Hour))
 
 	idleConnectionsClosed := make(chan struct{})
 	iris.RegisterOnInterrupt(func() {
@@ -68,23 +73,26 @@ func main() {
 	})
 
 	app.Get("{root:path}", func(ctx iris.Context) {
-		path := config.filesPath + "/files/" + ctx.Params().Get("root")
-		file, err := os.Stat(path)
-		fileHandle, err := os.Open(path)
+		path := config.storagePath + "/" + config.storageFolder + "/" + ctx.Params().Get("root")
+
+		fileInfo, err := os.Stat(path)
 
 		if err != nil {
 			ctx.NotFound()
+			return
 		}
 
-		defer fileHandle.Close()
+		file, _ := os.Open(path)
+		defer file.Close()
 
-		ctx.CompressWriter(true)
-		ctx.ServeContent(fileHandle, file.Name(), file.ModTime())
-		ctx.ServeFile(path)
+		_ = ctx.CompressWriter(true)
+		ctx.ServeContent(file, fileInfo.Name(), fileInfo.ModTime())
 	})
 
 	if config.production {
-		app.Run(iris.AutoTLS(":"+config.port, config.domain, config.email))
+		if err := app.Run(iris.AutoTLS(":"+config.port, config.domain, config.email)); err != nil {
+			log.Panic(err)
+		}
 	} else {
 		if err := app.Listen(":" + config.port); err != nil {
 			log.Panic(err)
